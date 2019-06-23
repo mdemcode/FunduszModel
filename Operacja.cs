0c7269abb0df44d7cd0e6daab023867c98f63b2e
Michael <45467061+mdemcode@users.noreply.github.com>
2019-06-23 15:09:36 +0200

    Add project files.

---
+++ Operacja.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunduszModel
+{
+    [Table ("Operacje")]
+    class Operacja
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime DataOperacji { get; set; }
+
+    }
+}
